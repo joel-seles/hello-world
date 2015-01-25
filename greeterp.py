d6e16894258ed6eacb157740744d3697a1434171
Joel Seles <joel-seles@users.noreply.github.com>
2015-01-25 16:08:35 -0500

    Create greeterp.py

---
+++ greeterp.py
@@ -0,0 +1,11 @@
+# greeterp.py .  
+# A short program demonstrating how to get user input.  This is one of the first lessons in Python Programming, Third Edition.  For the Absolute Beginner. By Michael Dawson.
+# Joel Seles , 25 January 2015
+
+name = input("Hello. What is your name?")
+
+print(name)
+
+print("Hello,", name)
+
+input("\n\nPress the Enter Key to exit.")
